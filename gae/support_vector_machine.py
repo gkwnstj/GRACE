0c4fe7ebdf3fd938a01fd54bc54960d69324aea1
gkwnstj <87974851+gkwnstj@users.noreply.github.com>
2022-09-17 00:22:02 +0900

    Update support_vector_machine.py

--- gae/support_vector_machine.py
+++ gae/support_vector_machine.py
@@ -6,7 +6,7 @@ from sklearn.svm import SVC
 
 
 def svm_m(names, PRED_elbow_kmean):
-    svm_file = pd.read_csv('C:/Users/inu/Desktop/GRACE/GRACE-main/Data/Norm_data_Umap/Norm_data_{}.txt'.format(names), sep='\t')
+    svm_file = pd.read_csv('.../Norm_data_Umap/Norm_data_{}.txt'.format(names), sep='\t')
     df_set = svm_file[['0','1']]
     svm_lb = du = pd.DataFrame(PRED_elbow_kmean, columns = ['label'])
     X_train, X_test, y_train, y_test = train_test_split(df_set, svm_lb, test_size = 0.2, random_state = 0)   ### auto shuffled
