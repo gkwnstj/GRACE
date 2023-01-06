4c1fe1074c9ca6cb901ba7a6922777bdb2fabf70
gkwnstj <87974851+gkwnstj@users.noreply.github.com>
2023-01-07 00:27:13 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,3 +23,41 @@ setup(name='gae',
       },
       package_data={'gae': ['README.md']},
       packages=find_packages())
+
+
+
+
+
+
+
+
+
+
+
+
+
+##from setuptools import setup
+##from setuptools import find_packages
+##
+##setup(name='gae',
+##      version='0.0.1',
+##      description='Implementation of (Variational) Graph Auto-Encoders in Tensorflow',
+##      author='Thomas Kipf',
+##      author_email='thomas.kipf@gmail.com',
+##      url='https://tkipf.github.io',
+##      download_url='https://github.com/tkipf/gae',
+##      license='MIT',
+##      install_requires=['pandas',
+##                        'numpy',
+##                        'tensorflow',
+##                        'networkx',
+##                        'scikit-learn',
+##                        'scipy',
+##                        'seaborn',
+##                        'yellowbrick'
+##                        ],
+##      extras_require={
+##          'visualization': ['matplotlib'],
+##      },
+##      package_data={'gae': ['README.md']},
+##      packages=find_packages())
